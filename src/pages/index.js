da45fdfea1afdf5752060b12e064a8ba6440ae71
unknown <dbrudne1@gmail.com>
2019-03-03 13:43:24 -0600

    test

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,15 @@
-import React from 'react'
-import { graphql } from 'gatsby'
-import get from 'lodash/get'
-import Helmet from 'react-helmet'
-import Hero from '../components/hero'
-import Layout from '../components/layout'
-import ArticlePreview from '../components/article-preview'
+import React from "react";
+import { graphql } from "gatsby";
+import get from "lodash/get";
+import Helmet from "react-helmet";
+import Hero from "../components/hero";
+import Layout from "../components/layout";
+import ArticlePreview from "../components/article-preview";
 
 class RootIndex extends React.Component {
-  render() {
-    return <div>hey</div>
-  }
+	render() {
+		return <div>hey2</div>;
+	}
 }
 
-export default RootIndex
+export default RootIndex;
