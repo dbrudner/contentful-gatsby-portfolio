d0ce0fea239e30485fa7948a9cc5cc9aeee6b08a
unknown <dbrudne1@gmail.com>
2019-03-03 13:38:44 -0600

    still testing

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,15 +1,15 @@
-import React from 'react'
-import { Link, graphql } from 'gatsby'
-import get from 'lodash/get'
-import Helmet from 'react-helmet'
-import styles from './blog.module.css'
-import Layout from '../components/layout'
-import ArticlePreview from '../components/article-preview'
+import React from "react";
+import { Link, graphql } from "gatsby";
+import get from "lodash/get";
+import Helmet from "react-helmet";
+import styles from "./blog.module.css";
+import Layout from "../components/layout";
+import ArticlePreview from "../components/article-preview";
 
 class BlogIndex extends React.Component {
-  render() {
-    return <div>hey2</div>
-  }
+	render() {
+		return <div>hey3</div>;
+	}
 }
 
-export default BlogIndex
+export default BlogIndex;
