7507f4b09430535ab77929425c6dda35e043b7e2
unknown <dbrudne1@gmail.com>
2019-03-06 19:17:25 -0600

    im so tired

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,18 +1,12 @@
 import React from "react";
 import Container from "./container";
+import Navigation from "./navigation";
 
-class Template extends React.Component {
-	render() {
-		const { location, children } = this.props;
-		let header;
-
-		let rootPath = `/`;
-		if (typeof __PREFIX_PATHS__ !== `undefined` && __PREFIX_PATHS__) {
-			rootPath = __PATH_PREFIX__ + `/`;
-		}
-
-		return <Container>{children}</Container>;
-	}
+export default function Layout(props) {
+	return (
+		<>
+			<Navigation />
+			<Container>{props.children}</Container>
+		</>
+	);
 }
-
-export default Template;
