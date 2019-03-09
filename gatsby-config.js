5dfef4a917187118b0f8f33eff670c2c7e6b75b0
unknown <dbrudne1@gmail.com>
2019-03-09 14:07:40 -0600

    removing node-gatsby.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,39 +1,39 @@
-if (process.env.NODE_ENV !== 'production') require('dotenv').config()
+if (process.env.NODE_ENV !== "production") require("dotenv").config();
 
-let contentfulConfig
+let contentfulConfig;
 
 try {
-  // Load the Contentful config from the .contentful.json
-  contentfulConfig = require('./.contentful')
+	// Load the Contentful config from the .contentful.json
+	contentfulConfig = require("./.contentful");
 } catch (_) {}
 
 // Overwrite the Contentful config with environment variables if they exist
 contentfulConfig = {
-  spaceId: process.env.CONTENTFUL_SPACE_ID,
-  accessToken: process.env.CONTENTFUL_DELIVERY_TOKEN,
-}
+	spaceId: process.env.CONTENTFUL_SPACE_ID,
+	accessToken: process.env.CONTENTFUL_DELIVERY_TOKEN,
+};
 
-const { spaceId, accessToken } = contentfulConfig
+const { spaceId, accessToken } = contentfulConfig;
 
 if (!spaceId || !accessToken) {
-  throw new Error(
-    'Contentful spaceId and the delivery token need to be provided.'
-  )
+	throw new Error(
+		"Contentful spaceId and the delivery token need to be provided.",
+	);
 }
 
 module.exports = {
-  siteMetadata: {
-    title: 'Gatsby Contentful starter',
-  },
-  pathPrefix: '/gatsby-contentful-starter',
-  plugins: [
-    'gatsby-transformer-remark',
-    'gatsby-transformer-sharp',
-    'gatsby-plugin-react-helmet',
-    'gatsby-plugin-sharp',
-    {
-      resolve: 'gatsby-source-contentful',
-      options: contentfulConfig,
-    },
-  ],
-}
+	siteMetadata: {
+		title: "Gatsby Contentful starter",
+	},
+	pathPrefix: "/gatsby-contentful-starter",
+	plugins: [
+		"gatsby-transformer-remark",
+		"gatsby-transformer-sharp",
+		"gatsby-plugin-react-helmet",
+		"gatsby-plugin-sharp",
+		{
+			resolve: "gatsby-source-contentful",
+			options: contentfulConfig,
+		},
+	],
+};
