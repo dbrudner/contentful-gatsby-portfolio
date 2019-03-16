e3ca01c6d34384c2fdb6c55fad89ab84215dce0e
unknown <dbrudne1@gmail.com>
2019-03-16 17:45:19 -0500

    updating images for blog pages

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,15 +1,20 @@
 import React from "react";
 import Layout from "../components/layout";
-import {
-	documentToHtmlString,
-	BLOCKS,
-} from "@contentful/rich-text-html-renderer";
+import { documentToHtmlString } from "@contentful/rich-text-html-renderer";
+import { BLOCKS } from "@contentful/rich-text-types";
+import styled from "styled-components";
+
+const BlogPost = styled.div`
+	img {
+		width: 100%;
+	}
+`;
 
 const Blog = props => {
-	console.log(props);
+	console.log(BLOCKS);
 	return (
 		<Layout>
-			<div
+			<BlogPost
 				dangerouslySetInnerHTML={{
 					__html: documentToHtmlString(props.pageContext.post.json, {
 						renderNode: {
@@ -17,12 +22,13 @@ const Blog = props => {
 								data: {
 									target: { fields },
 								},
-							}) =>
-								`<img src="${fields.file.url}" height="${
-									fields.file.details.image.height
-								}" width="${
-									fields.file.details.image.width
-								}" alt="${fields.description}"/>`,
+							}) => {
+								console.log(fields);
+								return `<img src="${
+									fields.file["en-US"].url
+								}" alt="${fields.description ||
+									fields.title["en-US"]}"/>`;
+							},
 						},
 					}),
 				}}
