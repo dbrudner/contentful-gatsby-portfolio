69b6fbcd7fa0b2cc9cc889a5ab27ac1b43af5b1c
Dave Brudner <dbrudne1@gmail.com>
2019-03-23 08:23:34 -0500

    idk

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,52 @@
+import { documentToHtmlString } from "@contentful/rich-text-html-renderer";
+import { BLOCKS } from "@contentful/rich-text-types";
+import { graphql } from "gatsby";
 import React from "react";
+import styled from "styled-components";
 import Layout from "../components/layout";
 
-const RootIndex = props => {
+const Content = styled.div`
+	img {
+		width: 100%;
+	}
+`;
+
+const Index = props => {
+	const { header, pageContent } = props.data.allContentfulPage.edges[0].node;
 	return (
 		<Layout>
-			<h1>brudnesrg</h1>
+			<h1>{header}</h1>
+			<Content
+				dangerouslySetInnerHTML={{
+					__html: documentToHtmlString(pageContent.json, {
+						renderNode: {
+							[BLOCKS.EMBEDDED_ASSET]: asset => {
+								return `<img src="${
+									fields.file["en-US"].url
+								}" alt="${fields.description ||
+									fields.title["en-US"]}"/>`;
+							}
+						}
+					})
+				}}
+			/>
 		</Layout>
 	);
 };
 
-export default RootIndex;
+export const pageQuery = graphql`
+	query {
+		allContentfulPage(filter: { header: { eq: "About" } }) {
+			edges {
+				node {
+					header
+					pageContent {
+						json
+					}
+				}
+			}
+		}
+	}
+`;
+
+export default Index;
